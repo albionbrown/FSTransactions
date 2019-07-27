f7c1a4d5f61f23f39a2e1a2e0c17d481a6c25283
Josh Brown <joshbrown8159@gmail.com>
2019-07-27 23:13:17 +0100

    Added new Action files

---
+++ src/Action/CopyFile.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace FSTransactions\Action;
+
+use FSTransactions;
+
+class Copy extends ActionBase {
+
+  /**
+   * The path of the file to copy
+   */
+  private $source;
+
+  /**
+   * The path to copy the source file to
+   */
+  private $destination;
+
+  public function __construct(string $source, string $destination) {
+
+    $this->source = $source;
+    $this->destination = $destination;
+  }
+
+  public function execute() {
+
+    /* @todo check that directories exist and PHP has 
+     * permission to write to the destination */
+
+    if (!copy($this->source, $this->destination)) {
+      throw new TransactionException("Failed to copy ");
+    }
+  }
+
+  /**
+   * Delete the copied file
+   */
+  public function reverse() {
+
+    if (!unlink($this->destination)) {
+      throw new RollbackFailureException("");
+    }
+  }
+}
