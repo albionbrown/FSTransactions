bea2284d29618dc1b06fed06570cf57ec5c56970
Josh Brown <joshbrown8159@gmail.com>
2019-07-27 23:16:35 +0100

    Checking if parameter of Transaction::addAction is an instance of \FSTransaction\Action\ActionBase and defined body of Transaction::commit()

--- src/Transaction.php
+++ src/Transaction.php
@@ -20,19 +20,41 @@ class Transaction
   }
 
   /**
+   * Rollback the transaction incase the object instance
+   * is destroyed before the transaction could be completed
+   */
+  public function __destruct() {
+
+    $this->rollback();
+  }
+
+  /**
    * Add an action to the transaction queue
    * 
    * @param FSTransaction\ActionBase $action The action to be performed during the transaction
    */
-  public function addAction(FSTransaction\ActionBase $action) {
+  public function addAction($action) {
 
+    if (!$action instanceof Action\ActionBase) {
+      throw new Exception("Action must be an instance of \FSTransactions\Action\ActionBase");
+    }
+    
     $this->queue->addToQueue($action);
   }
 
+  /** 
+   * Execute all actions in the transaction queue
+   * 
+   * @return void
+   */
   public function commit() {
- 
+    
+    foreach ($this->queue->getQueue() as $action) {
+
+      $action->execute();
+    }
   }
-  
+
   public function rollback() {
 
   }
