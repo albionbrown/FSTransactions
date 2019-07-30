1770752896ace294ed1e2707c973e1d7c9752ab4
Josh Brown <joshbrown8159@gmail.com>
2019-07-30 19:18:36 +0100

    Tests for CopyFileTest.php

--- tests/Actions/CopyFileTest.php
+++ tests/Actions/CopyFileTest.php
@@ -35,4 +35,8 @@ class CopyFileTest extends TestCase
 
     $this->assertEquals($sourceFileContents, $destinationFileContents);
   }
+
+  public function testCopyFileReverseSuccess() {
+    
+  }
 }
