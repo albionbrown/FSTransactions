08869c49e88cb82d6d82f9ee2e632316dc8ddd11
Josh Brown <joshbrown8159@gmail.com>
2019-07-30 23:20:16 +0100

    Defined function bodies in MoveFile.php

--- src/Action/MoveFile.php
+++ src/Action/MoveFile.php
@@ -19,13 +19,21 @@ class MoveFile extends Action
 
   public function __construct($source, $destination) {
 
+    $this->source = $source;
+    $this->destination = $destination;
   }
 
   public function execute() {
 
+    if (!rename($this->source, $this->destination)) {
+      throw new \FSTransactions\TransactionException("Failed to move {$this->source} to {$this->destination}");
+    }
   }
 
   public function reverse() {
     
+    if (!rename($this->destination, $this->source)) {
+      throw new \FSTransactions\RollbackFailureException("Failed to rollback. Could not move {$this->destination} to {$this->source}");
+    }
   }
 }
