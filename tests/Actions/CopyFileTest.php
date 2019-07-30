f342096ee7c71ffaac97cbbf2f5ea7785e872855
Josh Brown <joshbrown8159@gmail.com>
2019-07-30 19:11:40 +0100

    Added unit test file for testing CopyFile action

---
+++ tests/Actions/CopyFileTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use \FSTransactions\Action\CopyFile;
+
+class CopyFileTest extends TestCase
+{
+
+
+  private $destinationFilePath;
+
+  public function setUp() {
+
+    // Ensure source file exists
+  }
+
+  public function tearDown() {
+
+    unlink($this->destinationFilePath);
+  }
+
+  public function testCopyFileExecuteSuccess() {
+
+    $sourceFilePath = __DIR__.'/../test_files/CopyFileTestSource.txt';
+    $this->destinationFilePath = __DIR__.'/../test_files/CopyTestDestination.txt';
+
+    $action = new Copyfile($sourceFilePath, $this->destinationFilePath);
+    $action->execute();
+
+    $this->assertFileExists($this->destinationFilePath);
+
+    $sourceFileContents = file_get_contents($sourceFilePath);
+    $destinationFileContents = file_get_contents($this->destinationFilePath);
+
+    $this->assertEquals($sourceFileContents, $destinationFileContents);
+  }
+}
